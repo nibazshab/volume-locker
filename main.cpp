b84b0f3479d210b741c4c581c6aec01bad0dd9a3
ZShab Niba <44338441+nibazshab@users.noreply.github.com>
2025-10-28 15:00:57 +0800

    Improve robustness with com_ptr and HRESULT checks

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
-#include <windows.h>
-#include <mmdeviceapi.h>
 #include <endpointvolume.h>
+#include <mmdeviceapi.h>
+#include <windows.h>
 #include <winrt/Windows.Foundation.h>
 
 using namespace winrt;
@@ -10,52 +10,62 @@ const GUID VOLUME_CTX_GUID = {0x1a2b3c4d, 0x5e6f, 0x7a8b, {0x9c, 0x0d, 0x1e, 0x2
 
 class VolumeCallback : public winrt::implements<VolumeCallback, IAudioEndpointVolumeCallback>
 {
-public:
-    VolumeCallback(IAudioEndpointVolume *pVol) : m_pVol(pVol) {}
+  public:
+    VolumeCallback(winrt::com_ptr<IAudioEndpointVolume> pVol) : m_pVol(pVol) {}
 
     STDMETHODIMP OnNotify(PAUDIO_VOLUME_NOTIFICATION_DATA pNotify) override
     {
         if (!pNotify)
             return S_OK;
+
         if (pNotify->guidEventContext == VOLUME_CTX_GUID)
             return S_OK;
+
         if (pNotify->fMasterVolume == TARGET_VOLUME)
             return S_OK;
 
-        m_pVol->SetMasterVolumeLevelScalar(TARGET_VOLUME, &VOLUME_CTX_GUID);
+        HRESULT hr = m_pVol->SetMasterVolumeLevelScalar(TARGET_VOLUME, &VOLUME_CTX_GUID);
+        if (FAILED(hr))
+            return S_FALSE;
+
         return S_OK;
     }
 
-private:
-    IAudioEndpointVolume *m_pVol;
+  private:
+    winrt::com_ptr<IAudioEndpointVolume> m_pVol;
 };
 
-int WINAPI WinMain(
-    _In_ HINSTANCE,
-    _In_opt_ HINSTANCE,
-    _In_ LPSTR,
-    _In_ int)
+int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int)
 {
     init_apartment(apartment_type::multi_threaded);
 
-    auto pEndpointVol = []
-    {
-        winrt::com_ptr<IAudioEndpointVolume> endpoint;
-        winrt::com_ptr<IMMDeviceEnumerator> enumerator{winrt::create_instance<IMMDeviceEnumerator>(__uuidof(MMDeviceEnumerator))};
-        winrt::com_ptr<IMMDevice> device;
+    winrt::com_ptr<IMMDeviceEnumerator> enumerator;
+    HRESULT hr = CoCreateInstance(__uuidof(MMDeviceEnumerator), nullptr, CLSCTX_ALL, IID_PPV_ARGS(enumerator.put()));
+    if (FAILED(hr))
+        return 1;
 
-        enumerator->GetDefaultAudioEndpoint(eRender, eConsole, device.put());
-        device->Activate(__uuidof(IAudioEndpointVolume), CLSCTX_ALL, nullptr, endpoint.put_void());
-        return endpoint;
-    }();
+    winrt::com_ptr<IMMDevice> device;
+    hr = enumerator->GetDefaultAudioEndpoint(eRender, eConsole, device.put());
+    if (FAILED(hr))
+        return 2;
 
-    pEndpointVol->SetMasterVolumeLevelScalar(TARGET_VOLUME, &VOLUME_CTX_GUID);
-    auto callback = winrt::make_self<VolumeCallback>(pEndpointVol.get());
-    pEndpointVol->RegisterControlChangeNotify(callback.get());
+    winrt::com_ptr<IAudioEndpointVolume> pEndpointVol;
+    hr = device->Activate(__uuidof(IAudioEndpointVolume), CLSCTX_ALL, nullptr, pEndpointVol.put_void());
+    if (FAILED(hr))
+        return 3;
+
+    auto callback = winrt::make_self<VolumeCallback>(pEndpointVol);
+
+    hr = pEndpointVol->SetMasterVolumeLevelScalar(TARGET_VOLUME, &VOLUME_CTX_GUID);
+    if (FAILED(hr))
+        return 4;
+
+    hr = pEndpointVol->RegisterControlChangeNotify(callback.get());
+    if (FAILED(hr))
+        return 5;
 
     MSG msg;
-    while (GetMessageW(&msg, nullptr, 0, 0))
-    {
+    while (GetMessageW(&msg, nullptr, 0, 0)) {
         TranslateMessage(&msg);
         DispatchMessageW(&msg);
     }
